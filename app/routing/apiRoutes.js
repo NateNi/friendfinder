e8c7752881ee9c64c899c7c0ff816ea430832301
SSeal <nniesel90@gmail.com>
2018-11-02 23:53:49 -0500

    fixed routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 var friendsData = require("../data/friends");
 
 module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+    app.get("/app/friends", function(req, res) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
+    app.post("/app/friends", function(req, res) {
         friendsData.push(req.body);
     });
 }
