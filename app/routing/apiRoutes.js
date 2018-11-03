0fa1ad8513b526efa0ff9720b52476e6408c1c3b
SSeal <nniesel90@gmail.com>
2018-11-03 01:41:19 -0500

    Added the matching code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,18 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         friendsData.push(req.body);
-        res.json(friendsData);
+        let bestCompat = 100;
+        let bestIndex = 0;
+        for (let i = 0; i < friendsData.length-1; i++){
+            let compat = 0;
+            for (let j = 0; j < 10; j++){
+                compat = Math.abs(friendsData[i].scores[j]-friendsData[friendsData.length-1].scores[j]);
+            }
+            if (compat < bestCompat){
+                bestCompat = compat;
+                bestIndex = i;
+            }
+        }
+        res.json(friendsData[bestIndex]);
     });
 }
