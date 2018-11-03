0d563715a5dab27e029fee2c1c2cf33d500d147b
SSeal <nniesel90@gmail.com>
2018-11-02 23:46:52 -0500

    Fixed serverjs

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 
 require("./app/routing/apiRoutes")(app);
-require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
